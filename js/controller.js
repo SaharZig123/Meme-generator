ee6855b8f6228acd32ef2dfb534038fcd82b84b5
Sahar zigdon <saharzigdon123@gmail.com>
2021-10-27 20:43:28 +0300

    End of phase3

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,10 @@
-var gElCanvas = document.getElementById('my-canvas')
-var gCtx = gElCanvas.getContext('2d')
+const gElCanvas = document.getElementById('my-canvas')
+const gCtx = gElCanvas.getContext('2d')
 
-var elMain = document.querySelector('.main-content')
-var elEditor = document.querySelector('.meme-editor')
+const elMain = document.querySelector('.main-content')
+const elEditor = document.querySelector('.meme-editor')
 
+const meme = getMemeObject()
 var gCurrImg
 
 function onSelectImg(imgId) {
@@ -25,13 +26,31 @@ function drawImg(text) {
 }
 
 function drawText(text) {
-  let meme = getMemeObject()
+  let x = gElCanvas.width / 2
+  let fontSizes = getFontSizes()
+  console.log(fontSizes)
   gCtx.lineWidth = 2
   gCtx.strokeStyle = 'black'
   gCtx.fillStyle = 'white'
-  gCtx.font = `${getFontSize()}px Impact`
-  gCtx.fillText(text, 50, 50 + meme.lines[0].hight)
-  gCtx.strokeText(text, 50, 50 + meme.lines[0].hight)
+
+  gCtx.textAlign = 'center'
+  if (meme.selectedLineIdx === 0) {
+    gCtx.font = `${fontSizes[0]}px Impact`
+    gCtx.fillText(text, x, 50 + meme.lines[0].hight)
+    gCtx.strokeText(text, x, 50 + meme.lines[0].hight)
+    if (meme.lines[1]) {
+      gCtx.font = `${fontSizes[1]}px Impact`
+      gCtx.fillText(meme.lines[1].txt, x, 400 + meme.lines[1].hight)
+      gCtx.strokeText(meme.lines[1].txt, x, 400 + meme.lines[1].hight)
+    }
+  } else if (meme.selectedLineIdx === 1) {
+    gCtx.font = `${fontSizes[0]}px Impact`
+    gCtx.fillText(meme.lines[0].txt, x, 50 + meme.lines[0].hight)
+    gCtx.strokeText(meme.lines[0].txt, x, 50 + meme.lines[0].hight)
+    gCtx.font = `${fontSizes[1]}px Impact`
+    gCtx.fillText(text, x, 400 + meme.lines[1].hight)
+    gCtx.strokeText(text, x, 400 + meme.lines[1].hight)
+  }
 }
 
 function onAddText(textInput) {
@@ -40,25 +59,39 @@ function onAddText(textInput) {
 }
 
 function onIncreaseTextSize() {
-  let meme = getMemeObject()
   increaseTextSize()
-  drawImg(meme.lines[0].txt)
+  drawImg(meme.lines[meme.selectedLineIdx].txt)
 }
 
 function onDecreaseTextSize() {
-  let meme = getMemeObject()
   decreaseTextSize()
-  drawImg(meme.lines[0].txt)
+  drawImg(meme.lines[meme.selectedLineIdx].txt)
 }
 
 function onTextUp() {
-  let meme = getMemeObject()
-  meme.lines[0].hight -= 2
-  drawImg(meme.lines[0].txt)
+  meme.lines[meme.selectedLineIdx].hight -= 2
+  drawImg(meme.lines[meme.selectedLineIdx].txt)
 }
 
 function onTextDown() {
-  let meme = getMemeObject()
-  meme.lines[0].hight += 2
-  drawImg(meme.lines[0].txt)
+  meme.lines[meme.selectedLineIdx].hight += 2
+  drawImg(meme.lines[meme.selectedLineIdx].txt)
+}
+
+function onAddLine() {
+  if (meme.lines.length === 2) return
+  addNewLine()
+  let elSecondInput = document.querySelector('[name=text-line2]')
+  elSecondInput.hidden = false
+  meme.selectedLineIdx = 1
+  drawImg(elSecondInput.placeholder)
+}
+
+function onSwitchLines() {
+  switchLines()
+}
+
+function onClickInput(lineId) {
+  console.log(lineId)
+  setSelectedLine(lineId)
 }
--- js/service.js
+++ js/service.js
@@ -15,7 +15,7 @@ var gMeme = {
       size: 40,
       align: 'left',
       color: 'red',
-      hight:0
+      hight: 0,
     },
   ],
 }
@@ -27,7 +27,7 @@ function getSelectedImg(imgId) {
 }
 
 function addText(text) {
-  gMeme.lines[0].txt = text
+  gMeme.lines[gMeme.selectedLineIdx].txt = text
   console.log(gMeme)
 }
 
@@ -35,16 +35,35 @@ function getMemeObject() {
   return gMeme
 }
 
-function getFontSize() {
-  return gMeme.lines[0].size
+function getFontSizes() {
+  let fontSizes = [gMeme.lines[0].size]
+  if (gMeme.lines[1]) fontSizes = [gMeme.lines[0].size, gMeme.lines[1].size]
+  return fontSizes
 }
 
 function increaseTextSize() {
-  gMeme.lines[0].size += 3
+  gMeme.lines[gMeme.selectedLineIdx].size += 3
 }
 
 function decreaseTextSize() {
-  gMeme.lines[0].size -= 3
+  gMeme.lines[gMeme.selectedLineIdx].size -= 3
 }
 
+function addNewLine() {
+  let newLine = {
+    txt: '',
+    size: 40,
+    align: 'left',
+    color: 'red',
+    hight: 0,
+  }
+  gMeme.lines.push(newLine)
+}
 
+function switchLines() {
+  meme.selectedLineIdx = meme.selectedLineIdx ? 0 : 1
+}
+
+function setSelectedLine(lineId) {
+  gMeme.selectedLineIdx = +lineId
+}
