f304d81077ef29b7d271320a851a9fcdd75682f0
Sahar zigdon <saharzigdon123@gmail.com>
2021-10-27 15:16:11 +0300

    onClick open editor

--- js/controller.js
+++ js/controller.js
@@ -1,35 +1,39 @@
-var gElCanvas = document.getElementById('my-canvas');
-var gCtx = gElCanvas.getContext('2d');
+var gElCanvas = document.getElementById('my-canvas')
+var gCtx = gElCanvas.getContext('2d')
 
+var elMain = document.querySelector('.main-content')
+var elEditor = document.querySelector('.meme-editor')
 
-function onSelectImg(imgId){
-    getSelectedImg()
+var currImg
+
+function onSelectImg(imgId) {
+  let elPlaceHolder = document.querySelector('[name=text-line]').placeholder
+  currImg = getSelectedImg(imgId)
+  drawImg(elPlaceHolder)
+  elMain.hidden = true
+  elEditor.hidden = false
 }
 
-drawImg()
-
-
 function drawImg(text) {
-    var selectedImg=getSelectedImg()
-    var img = new Image();
-    img.src=selectedImg.url
-    img.onload = () => {
-      gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
-      drawText(text)
-    };
+  let selectedImg = currImg
+  let img = new Image()
+  img.src = selectedImg.url
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText(text)
   }
+}
 
+function drawText(text) {
+  gCtx.lineWidth = 2
+  gCtx.strokeStyle = 'black'
+  gCtx.fillStyle = 'white'
+  gCtx.font = '40px Impact'
+  gCtx.fillText(text, 50, 50)
+  gCtx.strokeText(text, 50, 50)
+}
 
-  function drawText(text) {
-    gCtx.lineWidth = 2;
-    gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = 'white';
-    gCtx.font = '40px Impact';
-    gCtx.fillText(text, 50, 50);
-    gCtx.strokeText(text, 50, 50);
-  }
-
-  function onAddText(textInput){
-      addText(textInput)
-      drawImg(textInput)
-  }
+function onAddText(textInput) {
+  addText(textInput)
+  drawImg(textInput)
+}
--- js/service.js
+++ js/service.js
@@ -1,13 +1,17 @@
 var gKeywords = { happy: 12, 'funny puk': 1 }
 
-var gImgs = [{ id: 1, url: '/meme-imgs/1.jpg', keywords: ['trump', 'angry', 'fake news'] }]
+var gImgs = [
+  { id: 1, url: '/meme-imgs/1.jpg', keywords: ['trump', 'angry', 'fake news'] },
+  { id: 2, url: '/meme-imgs/2.jpg', keywords: ['trump', 'angry', 'fake news'] },
+  { id: 3, url: '/meme-imgs/3.jpg', keywords: ['trump', 'angry', 'fake news'] },
+]
 
 var gMeme = {
   selectedImgId: 1,
   selectedLineIdx: 0,
   lines: [
     {
-      txt: 'I never eat Falafel',
+      txt: '',
       size: 20,
       align: 'left',
       color: 'red',
@@ -15,13 +19,13 @@ var gMeme = {
   ],
 }
 
-function getSelectedImg() {
-  // let imgIdx=gImgs.findIndex((img)=>imgId=gImgs.id)
-  return gImgs[0]
+function getSelectedImg(imgId) {
+  let imgIdx = gImgs.findIndex((img) => +imgId === img.id)
+  console.log(imgIdx)
+  return gImgs[imgIdx]
 }
 
-
-function addText(text){
-    gMeme.lines[0].txt=text
-    console.log(gMeme)
+function addText(text) {
+  gMeme.lines[0].txt = text
+  console.log(gMeme)
 }
